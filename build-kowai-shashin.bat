cd46359f2ebe1e5ceb8d2524a813b03f9c287e07
ArcaneAria <arcanearia@gmail.com>
2021-07-12 17:50:58 -0700

    Added the formatting step to the build process. Tweaked the tutorial screen to be more clean.

---
+++ build-kowai-shashin.bat
@@ -0,0 +1,11 @@
+:: If you want to move this bat to somewhere like your desktop, uncomment
+:: the pushd and change it to where your files are stored
+:: pushd "C:\Users\yagen\Desktop\Translation Working\Kowai Shashin\tools"
+
+call 0_format.bat
+call 1_insert.bat
+call 2_build.bat
+::popd
+
+echo "Build complete!"
+pause
